19f1f7d80bc071abb785864ceea3de939671f4d0
Natthawat Raocharoensinp <chan1sook@hotmail.com>
2023-05-28 20:23:34 +0700

    Fix bugs

--- server/api/topic/approve/[id].post.ts
+++ server/api/topic/approve/[id].post.ts
@@ -46,8 +46,8 @@ export default defineEventHandler(async (event) => {
 
   if(dayjs().diff(topicDoc.voteStartAt) > 0) {
     const diff = dayjs().diff(topicDoc.voteStartAt);
-    topicDoc.voteStartAt = new Date();
-    topicDoc.voteExpiredAt = dayjs().add(diff).toDate();
+    topicDoc.voteStartAt = dayjs().add(1, "hours").toDate();
+    topicDoc.voteExpiredAt = dayjs(topicDoc.voteStartAt).add(diff).toDate();
   }
 
   if(topicDoc.notifyVoter &&  topicDoc.status === "approved") {
