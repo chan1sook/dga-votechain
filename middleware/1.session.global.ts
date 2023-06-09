20a404a0362efce87acedbf2ccbf4dbc430ce40e
Natthawat Raocharoensinp <chan1sook@hotmail.com>
2023-06-09 14:50:16 +0700

    Fix F0023 bug

--- middleware/1.session.global.ts
+++ middleware/1.session.global.ts
@@ -1,13 +1,35 @@
+import { getDefaultAdminTopMenus, getDefaultDevTopMenus, getDefaultTopMenus } from "~/src/services/form/preference";
+
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const { data } = await useFetch("/api/session/get");
 
   if(data.value) {
+    const preferences = {
+      topMenus: getDefaultTopMenus(),
+      adminTopMenus: getDefaultAdminTopMenus(),
+      devTopMenus: getDefaultDevTopMenus(),
+    }
+    if(data.value.preferences) {
+      if(Array.isArray(data.value.preferences.topMenus)) {
+        preferences.topMenus = data.value.preferences.topMenus;
+      }
+
+      if(Array.isArray(data.value.preferences.adminTopMenus)) {
+        preferences.topMenus = data.value.preferences.adminTopMenus;
+      }
+
+      if(Array.isArray(data.value.preferences.devTopMenus)) {
+        preferences.topMenus = data.value.preferences.devTopMenus;
+      }
+    }
+
+
     useSessionData().value = {
       userid: data.value.userid,
       hasCitizenId: data.value.hasCitizenId,
       permissions: data.value.permissions || [],
       isGovOfficer: data.value.isGovOfficer,
-      preferences: data.value.preferences,
+      preferences,
       roleMode: data.value.roleMode,
       firstName: data.value.firstName,
       lastName: data.value.lastName,
@@ -22,7 +44,9 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
       hasCitizenId: undefined,
       permissions: [],
       preferences: {
-        topMenus: [],
+        topMenus: getDefaultTopMenus(),
+        adminTopMenus: getDefaultAdminTopMenus(),
+        devTopMenus: getDefaultDevTopMenus(),
       },
       isGovOfficer: undefined,
       roleMode: "guest",
