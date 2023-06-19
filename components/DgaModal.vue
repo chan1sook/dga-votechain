fc43ebc430a5e790a7cbbb63fff1b0c78bb246da
Natthawat Raocharoensinp <chan1sook@hotmail.com>
2023-06-19 19:14:48 +0700

    (Temp) Revert Modal

--- components/DgaModal.vue
+++ components/DgaModal.vue
@@ -6,18 +6,16 @@
     leave-to-class="opacity-0"
   >
     <div v-if="props.show" class="z-[1000] fixed inset-0 p-2 sm:p-4 bg-gray-500/50 flex flex-row items-center justify-center" @click="backdropClose">
-      <div class="w-full h-full overflow-y-auto">
-        <div class="bg-dga-blue rounded-lg p-6 text-white flex flex-col gap-4" @click.stop>
-          <slot>Modal</slot>
-          <div v-if="!props.hideButtons" class="flex flex-row items-center justify-center gap-4">
-            <template v-if="props.closeOnly">
-              <DgaButton color="dga-orange" :title="$t('app.modal.close')" @click="emit('close')">{{ $t('app.modal.close') }}</DgaButton>
-            </template>
-            <template v-else>
-              <DgaButton color="dga-orange" :title="$t('app.modal.cancel')" @click="emit('cancel')">{{ $t('app.modal.cancel') }}</DgaButton>
-              <DgaButton color="green" :title="$t('app.modal.confirm')" @click="emit('confirm')">{{ $t('app.modal.confirm')}}</DgaButton>
-            </template>
-          </div>
+      <div class="bg-dga-blue rounded-lg p-6 text-white flex flex-col gap-4" @click.stop>
+        <slot>Modal</slot>
+        <div v-if="!props.hideButtons" class="flex flex-row items-center justify-center gap-4">
+          <template v-if="props.closeOnly">
+            <DgaButton color="dga-orange" :title="$t('app.modal.close')" @click="emit('close')">{{ $t('app.modal.close') }}</DgaButton>
+          </template>
+          <template v-else>
+            <DgaButton color="dga-orange" :title="$t('app.modal.cancel')" @click="emit('cancel')">{{ $t('app.modal.cancel') }}</DgaButton>
+            <DgaButton color="green" :title="$t('app.modal.confirm')" @click="emit('confirm')">{{ $t('app.modal.confirm')}}</DgaButton>
+          </template>
         </div>
       </div>
     </div>
